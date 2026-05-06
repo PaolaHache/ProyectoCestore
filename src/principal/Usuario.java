73bb1a489402e41924baf7493dc560fceea193f8
Paola <paola291187@gmail.com>
2026-05-05 23:47:27 -0300

    Commit and Push

---
+++ src/principal/Usuario.java
@@ -0,0 +1,34 @@
+package principal;
+
+public class Usuario {
+    public static void main(String[] args) {
+
+        System.out.println("Declarar atributos básicos");
+
+        String nombre = "Yanis";
+        String apellido = "Correa";
+        String email = "yaniscorrea@gmail.com";
+        String pais = "Uruguay";
+        String perfil = "administrador";
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Email: " + email);
+        System.out.println("Pais: " + pais);
+        System.out.println("Perfil: " + perfil);
+
+
+
+
+        //Funcionalidad, descripción y datos solicitados
+
+       // Login. Iniciar sesión como administrador. Email y contraseña
+        // Reiniciar contraseña. Recuperar cuenta. Email, contraseña y repetir contraseña
+        // Crear cuenta administrador. Dar alta de cuenta para administrador. Nombre, apellido, email, contraseña, repetir contraseña y país de nacimiento
+        // Crear cuenta tester. Dar alta de cuenta para tester. Nombre, apellido, email, país de nacimiento, contraseña por defecto y rango (tester junior, tester senior, tester líder)
+        // Ver usuarios. Ver usuarios registrados. Nombre, apellido, email, país, perfil (rango de tester o administrador) 5.1. Eliminar usuarios. Eliminar usuarios registrados en el sistema. Aceptar o cancelar
+        // Ver perfil de usuario. Ver detalles del perfil. Nombre, apellido, email, país, perfil 6.1. Modificar datos de perfil de usuario. Cambiar detalles del perfil. Nombre, apellido, email, país, perfil
+        // Logout. Cerrar sesión. Sí o cancelar
+
+    }
+}
