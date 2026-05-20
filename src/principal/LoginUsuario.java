deafa696050d10ff7288e2c731f919cc0426850c
Paola <paola291187@gmail.com>
2026-05-19 21:14:13 -0300

    Tarea 2

--- src/principal/LoginUsuario.java
+++ src/principal/LoginUsuario.java
@@ -1,11 +1,14 @@
 package principal;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LoginUsuario {
     public static void main(String[] args) {
-        String mail = "yaniscorrea@gmail.com";
-        String pass = "12345";
+        // Lista de administradores
+        ArrayList<Usuario> admins = new ArrayList<>();
+        admins.add(new Usuario("Yanis", "Correa", "yaniscorrea@gmail.com", "Uruguay", "ADMIN", "12345"));
+        admins.add(new Usuario("Leonardo", "Pérez", "leonardoperez@gmail.com", "Uruguay", "ADMIN", "12345"));
 
         Scanner scanner = new Scanner(System.in);
 
@@ -15,14 +18,17 @@ public class LoginUsuario {
         System.out.println("Ingrese su contraseña: ");
         String inputContrasena = scanner.nextLine();
 
-        if (inputEmail.equals(mail)) {
-            if (inputContrasena.equals(pass)) {
-                System.out.println("Sesión iniciada");
-            } else {
-                System.out.println("Contraseña incorrecta");
+        boolean acceso = false;
+        for (Usuario u : admins) {
+            if (u.email.equals(inputEmail) && u.password.equals(inputContrasena)) {
+                System.out.println("Sesión iniciada correctamente");
+                acceso = true;
+                break;
             }
-        } else {
-            System.out.println("Usuario incorrecto");
+        }
+
+        if (!acceso) {
+            System.out.println("Credenciales incorrectas");
         }
     }
 }
